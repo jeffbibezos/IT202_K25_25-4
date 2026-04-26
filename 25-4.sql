5334b9972abf8479937331fe56cac561188d322f
jeffbibezos <danhc7620@gmail.com>
2026-04-26 22:29:41 +0700

    second commit

--- 25-4.sql
+++ 25-4.sql
@@ -1,3 +1,4 @@
+-- 25/4
 create database BookWorm;
 
 use BookWorm;
@@ -26,5 +27,39 @@ create table Customers (
     phone varchar(15) not null unique,
 	registration_date date default(current_date)
 );
+
+-- 26/4
+insert into Authors (full_name, birth_year, nationality) 
+values 
+('Nguyễn Nhật Ánh', 1955, 'Việt Nam'),
+('Agatha Christie', 1890, 'Anh'),
+('Dale Carnegie', 1888, 'Mỹ');
+
+insert into Books (book_name, category, price, publish_year, author_id) 
+values 
+('Cho tôi xin một vé đi tuổi thơ', 'Văn học', 85000, 2008, 1),
+('Mắt biếc', 'Văn học', 110000, 1990, 1),
+('Kính vạn hoa', 'Văn học', 55000, 1995, 1),
+('Án mạng trên chuyến tàu cao tốc Phương Đông', 'Trinh thám', 120000, 1934, 2),
+('Vụ ám thát Roger Ackroyd', 'Trinh thám', 95000, 1926, 2),
+('Đắc Nhân Tâm', 'Kỹ năng', 76000, 1936, 3),
+('Quẳng gánh lo đi và vui sống', 'Kỹ năng', 82000, 1948, 3),
+('Linh hồn của tiền', 'Kỹ năng', 150000, 2021, 3);
+
+insert into Customers (customer_name, email, phone)
+values 
+('Trần Văn An', 'an.tran@gmail.com', '0901234567'),
+('Lê Thị Bình', 'binh.le@yahoo.com', '0912345678'),
+('Phạm Hồng Cường', 'cuong.pham@outlook.com', '0987654321'),
+('Nguyễn Minh Dung', 'dung.nguyen@hotmail.com', '0933445566'),
+('Đỗ Hoàng Yến', 'yen.do@protonmail.com', '0944556677');
+
+
+insert into Customers (customer_name, email, phone)
+value
+('Trần Văn An', 'an.tran@gmail.com', '0901234567');
+-- Nó báo lỗi Duplicate bởi vì ban đầu ở bảng Customers chúng ta đã có unique nó bắt buộc
+-- mỗi khách hàng phải có gmail riêng và không trùng nhau
+
 
 
